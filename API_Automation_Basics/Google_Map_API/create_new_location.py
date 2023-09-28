28996810f2ebbb8c10a07bba42725bd0959965a9
Maiia Orlovsky <orlovsky-maya@mail.ru>
2023-09-28 09:34:02 +0400

    Added files: - deleting_location_body - update_location_body Updated file: - create_new_location file

--- API_Automation_Basics/Google_Map_API/create_new_location.py
+++ API_Automation_Basics/Google_Map_API/create_new_location.py
@@ -1,17 +1,19 @@
 import json
-
 import requests
+
+""" CRUD operations for new location"""
 
 
 class TestNewLocation:
     """Creating new location"""
 
     def test_create_location(self):
-        """Getting post url"""
-
         base_url = 'https://rahulshettyacademy.com'
-        post_resource = '/maps/api/place/add/json'
         key = '?key=qaclick123'
+
+        # Getting post url
+
+        post_resource = '/maps/api/place/add/json'
         post_url = base_url + post_resource + key
         print(post_url)
 
@@ -67,6 +69,156 @@ class TestNewLocation:
         print(f'Correct status code: {status_code}')
 
         print('New location is created successfully')
+
+        """Updating new location"""
+
+        """PUT Request - updating location"""
+
+        # Getting put url
+
+        put_resource = '/maps/api/place/update/json'
+        put_url = base_url + put_resource + key
+        print(put_url)
+
+        # Open json file
+
+        with open('update_location_body.json', 'r', encoding='utf-8') as body_json:
+            body_json = json.load(body_json)
+            body_json['place_id'] = place_id
+
+        # PUT Request
+
+        put_result = requests.put(put_url, json=body_json)
+
+        # Checking status_code
+
+        status_code = put_result.status_code
+        assert status_code == 200, 'Incorrect status code, status code should be 200'
+        print(f'Correct status code: {status_code}')
+
+        # Checking message
+
+        info_json = put_result.json()
+        message_success = info_json.get('msg')
+        assert message_success == 'Address successfully updated', 'Incorrect message'
+        print(f'Correct success message: {message_success}')
+
+        """GET Request - Checking updated location """
+
+        # GET request
+
+        print(get_url)
+        get_result = requests.get(get_url)
+        get_result.encoding = 'utf-8'
+        print(get_result.text)
+
+        # Checking status_code
+
+        status_code = get_result.status_code
+        assert status_code == 200, 'Incorrect status code, status code should be 200'
+        print(f'Correct status code: {status_code}')
+
+        # Checking new address
+
+        info_json = get_result.json()
+        address = info_json.get('address')
+        assert address == '100 Lenina street, RU', 'Incorrect new address'
+        print(f'New address of location: {address}')
+        print('The location is updated successfully')
+
+        """Deleting new location"""
+
+        """DELETE Request - deleting location"""
+
+        # Getting delete url
+
+        delete_resource = '/maps/api/place/delete/json'
+        delete_url = base_url + delete_resource + key
+        print(delete_url)
+
+        # Open json file
+
+        with open('deleting_location_body.json', 'r', encoding='utf-8') as body_json:
+            body_json = json.load(body_json)
+            body_json['place_id'] = place_id
+
+        # DELETE request
+
+        delete_result = requests.get(delete_url, json=body_json)
+
+        # Checking status_code
+
+        status_code = delete_result.status_code
+        assert status_code == 200, 'Incorrect status code, status code should be 200'
+        print(f'Correct status code: {status_code}')
+
+        # Checking message
+
+        info_json = delete_result.json()
+        status = info_json.get('status')
+        assert status == "OK", 'Incorrect message'
+        print(f'Correct success message: {status}')
+        print('Location is deleted successfully')
+
+        """GET Request - Checking deleted location """
+
+        # Getting get url
+
+        get_url = base_url + get_resource + key + '&place_id=' + place_id
+        print(get_url)
+
+        # GET request
+
+        get_result = requests.get(get_url)
+        get_result.encoding = 'utf-8'
+        print(get_result.text)
+
+        # Checking status_code
+
+        status_code = get_result.status_code
+        assert status_code == 404, 'Incorrect status code, status code should be 404'
+        print(f'Correct status code: {status_code}')
+
+        # Checking message
+
+        info_json = get_result.json()
+        message_success = info_json.get('msg')
+        assert message_success == "Get operation failed, looks like place_id  doesn't exists", 'Incorrect message'
+        print(f'Correct success message: {message_success}')
+
+        """Updating non-existent location"""
+
+        """PUT Request - updating non-existent location"""
+
+        # Getting put url
+
+        put_resource = '/maps/api/place/update/json'
+        put_url = base_url + put_resource + key
+        print(put_url)
+
+        # Open json file
+
+        with open('update_location_body.json', 'r', encoding='utf-8') as body_json:
+            body_json = json.load(body_json)
+            body_json['place_id'] = place_id
+
+        # PUT Request
+
+        put_result = requests.put(put_url, json=body_json)
+
+        # Checking status_code
+
+        status_code = put_result.status_code
+        assert status_code == 404, 'Incorrect status code, status code should be 404'
+        print(f'Correct status code: {status_code}')
+
+        # Checking message
+
+        info_json = put_result.json()
+        message_success = info_json.get('msg')
+        assert message_success == "Update address operation failed, looks like the data doesn't exists", \
+            'Incorrect message'
+        print(f'Correct success message: {message_success}')
 
 
 location = TestNewLocation()
